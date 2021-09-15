220a256a2440fa39bffa8cdacafe3f376db2808d
Manuel Bovo <manuel.bovo@gmail.com>
2021-09-15 16:14:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# PDH - PagerDuty CLI for humans
+
+![Build Image](https://github.com/mbovo/pdh/actions/workflows/build-image.yml/badge.svg)
+![Publishing Doc](https://github.com/mbovo/pdh/actions/workflows/doc-publish.yaml/badge.svg)
+
+See [docs](./docs)
+
+## Requirements
+
+- [Taskfile](https://taskfile.dev)
+- [pre-commit](https://pre-commit.com)
+- Python >=3.9
+- Docker
---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r") as f:
+    requires = f.read().splitlines()
+
+with open("test-requirements.txt", "r") as f:
+    test_requires = f.read().splitlines()
+
+
+with open("version", "r") as f:
+    version = f.read()
+
+setuptools.setup(
+    name="cloudtools",
+    version=version,
+    author="Manuel Bovo",
+    author_email="manuel.bovo@gmail.com",
+    description="PagerDuty cli for humans",
+    long_description=long_description,
+    url="",
+    install_requires=requires,
+    extras_require={
+        "test": test_requires,
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Linux",
+    ],
+    python_requires=">=3.9",
+    entry_points={
+        "console_scripts": [
+            "pdh=pd.main:main",
+        ]
+    },
+)
