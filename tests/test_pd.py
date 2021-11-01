027075788dc593f9899d58d0b8c32e884753dec1
Manuel Bovo <manuel.bovo@gmail.com>
2021-11-01 12:49:55 +0100

    [WIP] Improve coverage

---
+++ tests/test_pd.py
@@ -0,0 +1,179 @@
+from typing import Dict, List
+import pytest
+from pytest_mock import MockerFixture
+import json
+
+from requests.models import Response
+from pdh import pd
+
+
+class FakePD(object):
+    def list_all(*args, **kwargs) -> List:
+        ret = []
+        loaded = []
+
+        with open("tests/incidents_list.json", "r") as f:
+            loaded = json.load(f)
+
+        if "user_ids[]" in kwargs["params"]:
+            for i in ret:
+                for assignments in i["assignments"]:
+                    if assignments["assignee"]["id"] in kwargs["params"]["user_ids[]"]:
+                        ret.append(i)
+
+        if "statuses[]" in kwargs["params"]:
+            for i in loaded:
+                if i["status"] in kwargs["params"]["statuses[]"]:
+                    ret.append(i)
+        if "urgencies[]" in kwargs["params"]:
+            for i in loaded:
+                if i["urgency"] in kwargs["params"]["urgencies[]"]:
+                    ret.append(i)
+
+        return ret
+
+    def iter_all(*args, **kwargs) -> List:
+        loaded = []
+        if args[1] == "users":
+            with open("tests/users_list.json", "r") as f:
+                loaded = json.load(f)
+        return loaded
+
+    def put_incident(addr: str, json: list):
+        return json
+
+    def get(addr: str, **kwargs) -> Response:
+        return Response()
+
+    def post(addr: str, json: dict) -> Response:
+        return Response()
+
+    def rput(*argv, **kwargs) -> Response:
+        return Response()
+
+    def rget(*argv, **kawargs) -> List:
+        loaded = []
+        ret = []
+        if "users" in argv[0]:
+            uid = argv[0].replace("/users/")
+            with open("tests/users_list.json", "r") as f:
+                loaded = json.load(f)
+                for u in loaded:
+                    if u["id"] == uid:
+                        ret.append(u)
+        if "incidents" in argv[0]:
+            id = argv[0].replace("/incidents/", "")
+            with open("tests/incidents_list.json", "r") as f:
+                loaded = json.load(f)
+            for i in loaded:
+                if i["id"] == id:
+                    ret.append(i)
+
+        return ret
+
+
+@pytest.fixture
+def config() -> Dict:
+    # This is the test token from https://developer.pagerduty.com/api-reference
+    return {"apikey": "y_NbAkKc66ryYTWUXYEu", "email": "user@domain.tld", "uid": "PXCT22H"}
+
+
+@pytest.fixture
+def incidents(mocker: MockerFixture, config: Dict) -> pd.Incidents:
+    mocker.patch("pdpyras.APISession.iter_all", side_effect=FakePD.iter_all)
+    mocker.patch("pdpyras.APISession.rget", side_effect=FakePD.rget)
+    mocker.patch("pdpyras.APISession.list_all", side_effect=FakePD.list_all)
+    mocker.patch("pdpyras.APISession.rput", side_effect=FakePD.rput)
+    mocker.patch("pdpyras.APISession.get", side_effect=FakePD.get)
+    mocker.patch("pdpyras.APISession.post", side_effect=FakePD.post)
+    return pd.Incidents(config)
+
+
+@pytest.fixture
+def users(mocker: MockerFixture, config: Dict) -> pd.Users:
+    mocker.patch("pdpyras.APISession.iter_all", side_effect=FakePD.iter_all)
+    mocker.patch("pdpyras.APISession.rget", side_effect=FakePD.rget)
+    mocker.patch("pdpyras.APISession.list_all", side_effect=FakePD.list_all)
+    mocker.patch("pdpyras.APISession.rput", side_effect=FakePD.rput)
+    mocker.patch("pdpyras.APISession.get", side_effect=FakePD.get)
+    mocker.patch("pdpyras.APISession.post", side_effect=FakePD.post)
+    return pd.Users(config)
+
+
+def test_list_incidents(incidents: pd.Incidents, config: Dict):
+    incs = incidents.list()
+    assert incs is not None
+    assert len(incs) > 0
+    assert incs[0]["assignments"][0]["assignee"]["id"] == config["uid"]
+
+
+def test_list_my_incidents(incidents: pd.Incidents, config: Dict):
+    incs = incidents.mine()
+    assert incs is not None
+    assert len(incs) > 0
+    for i in incs:
+        assert i["assignments"][0]["assignee"]["id"] == config["uid"]
+
+
+def test_get_incident(incidents: pd.Incidents):
+    inc = incidents.get("Q0VVEEB5HX4U06")
+    assert inc is not None
+    assert len(inc) > 0
+    assert inc[0]["id"] == "Q0VVEEB5HX4U06"
+
+
+def test_ack(incidents: pd.Incidents):
+    inc = incidents.get("Q0VVEEB5HX4U06")
+    assert inc is not None
+    incidents.ack(inc)
+    assert inc[0]["status"] == pd.STATUS_ACK
+
+
+def test_resolve(incidents: pd.Incidents):
+    inc = incidents.get("Q0VVEEB5HX4U06")
+    assert inc is not None
+    incidents.resolve(inc)
+    assert inc[0]["status"] == pd.STATUS_RESOLVED
+
+
+def test_snooze(incidents: pd.Incidents):
+    inc = incidents.get("Q0VVEEB5HX4U06")
+    assert inc is not None
+    incidents.snooze(inc, 6000)
+    # TODO: verify
+
+
+def test_reassign(incidents: pd.Incidents, config: Dict):
+    inc = incidents.get("Q0VVEEB5HX4U06")
+    assert inc is not None
+    # Reassign to me
+    incidents.reassign(inc, uids=["1P3E4F"])
+    # TODO: verify
+
+
+# def test_list_users(users: pd.Users, config):
+#     assert False
+
+
+# def test_get_users(users: pd.Users):
+#     assert False
+
+
+# def test_search(users: pd.Users):
+#     assert False
+
+
+# def test_filter(users: pd.Users):
+#     assert False
+
+
+# def test_userIDs(users: pd.Users):
+#     assert False
+
+
+# def test_userID_by_mail(users: pd.Users):
+#     assert False
+
+
+# def test_userID_by_name(users: pd.Users):
+#     assert False
