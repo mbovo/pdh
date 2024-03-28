0341d6c0c90e1fbc9e0d0a5d3b37fbefeead21f9
Manuel Bovo <manuel.bovo@gmail.com>
2024-03-28 22:42:56 +0100

    Testing building a nix pkg

--- flake.nix
+++ flake.nix
@@ -6,9 +6,13 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, ... }@inputs:
     flake-utils.lib.eachSystem [
       "aarch64-darwin"
       "x86_64-darwin"
@@ -17,21 +21,30 @@
       (system:
         let
           pkgs = import nixpkgs { inherit system; };
+          p2nix = import poetry2nix { inherit pkgs; };
+          override = p2nix.defaultPoetryOverrides.extend
+                            (self: super: {
+                              pdpyras = super.pdpyras.overridePythonAttrs
+                              (
+                                old: {
+                                  buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+                                }
+                              );
+                            });
+          myappEnv = p2nix.mkPoetryEnv {
+                        projectDir = ./.;
+                        editablePackageSources = {
+                            my-app = ./src;
+                        };
+                        overrides = override;
+                      };
         in
         {
-          devShell = pkgs.mkShell {
-            buildInputs = [
-              pkgs.pre-commit
-              pkgs.go-task
-              pkgs.python311
-              pkgs.poetry
-              pkgs.docker
-            ];
-            shellHook = ''
-                task setup
-                source .venv/bin/activate
-                '';
+          packages.default = p2nix.mkPoetryApplication {
+            projectDir = ./.;
+            overrides = override;
           };
+          devShell = myappEnv.env;
         }
       );
 }
