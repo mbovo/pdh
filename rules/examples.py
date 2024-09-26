249b7adc4bbf72b159b78d568828dca0776be157
Manuel Bovo <manuel.bovo@gmail.com>
2024-09-26 13:00:39 +0200

    Update copyright section

--- rules/examples.py
+++ rules/examples.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # This file is part of the pdh (https://github.com/mbovo/pdh).
-# Copyright (c) 2020-2023 Manuel Bovo.
+# Copyright (c) 2020-2024 Manuel Bovo.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
